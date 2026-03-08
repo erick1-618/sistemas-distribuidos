a3714cb85f7de29171a41a978ad1b06ad0fa4d6f
erick1-618 <erickcefetbcc@gmail.com>
2026-03-08 11:54:39 -0300

    fix: thread encapsulation class, 5 threads execution

--- trab1/server.py
+++ trab1/server.py
@@ -2,10 +2,18 @@ import socket
 from threading import Thread
 from random import choice
 
-def reqJob(t_name, conn):
-    data = conn.recv(1024)
-    conn.sendall(f'Sinal recebido pela Thread {t_name}: {data.decode()}'.encode())
-    conn.close()
+class ReqJob(Thread):
+    def __init__(self, t_name):
+        super().__init__(name=t_name)
+        self.t_name = t_name
+
+    def start(self, conn):
+        data = conn.recv(1024)
+        conn.sendall(f'Sinal recebido por {self.t_name}: {data.decode()}'.encode())
+        conn.close()
+
+# Inicializando as threads
+thread_pool = [ReqJob('Thread-1'), ReqJob('Thread-2'), ReqJob('Thread-3'), ReqJob('Thread-4'), ReqJob('Thread-5')]
 
 server = socket.socket()
 
@@ -16,13 +24,6 @@ server.listen()
 while True:
     conn, addr = server.accept()
 
-    # Inicializando as threads que irão responder as requisições
-    t1 = Thread(target=reqJob, name='T1', args=('T1', conn))
-    t2 = Thread(target=reqJob, name='T2', args=('T2', conn))
-    t3 = Thread(target=reqJob, name='T3', args=('T3', conn))
-
-    thread_pool = (t1, t2, t3)
-
     t_selected = choice(thread_pool)
 
-    t_selected.start()
+    t_selected.start(conn)
